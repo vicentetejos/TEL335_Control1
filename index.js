6d92e9698a9867a96f6d5f86e9f7eb02dba56809
vicentetejos1 <vicente.tejos@usm.cl>
2024-04-22 23:34:32 -0400

    Fix Errors, Upgrade Logic promotions

--- index.js
+++ index.js
@@ -25,49 +25,44 @@ const promotion_rules = [
     n : 1
     }
     ]
-
     for (let i=0; i< promotion_rules.length; i++){
         if (promotion_rules[i].rule=="Nx$"){
             var cantidad_NProducts = promotion_rules[i].n;
             var porcentaje_N = promotion_rules[i].discount_percentage;
           
-        }else{
+        }if(promotion_rules[i].rule=="AyA" ){
             var cantidad_AyA= promotion_rules[i].n;
             var porcentaje_AyA= promotion_rules[i].discount_percentage;
          
         }
+        else{
+            continue
+        }
     }
-    
-  function promotionNx$(amount, unit_base_price){
+function promotionNx$(amount, unit_base_price){
     let total_final=0;
-    var total = amount*unit_base_price;
-    var descuento = total*porcentaje_N/100;
-    total_final = total - descuento;
+    total_final=cantidad_NProducts*Math.trunc(amount/cantidad_NProducts)*unit_base_price*(1-(porcentaje_N/100)) + (amount%cantidad_NProducts)*unit_base_price;
     return total_final;
     }
-    function promotionAyA(amount, unit_base_price){
+function promotionAyA(amount, unit_base_price){
     let total_final=0;
-    var total = amount*unit_base_price;
-    var descuento = total*porcentaje_AyA/100;
-    total_final = total - descuento;
+    total_final=cantidad_AyA*Math.trunc(amount/cantidad_AyA)*unit_base_price*(1-(porcentaje_AyA/100)) + (amount%cantidad_AyA)*unit_base_price;
     return total_final;
     }
-
    
     // format data cart_id e items
     // item_id, promotion, amount, unit_base_price
     app.post('/api/get-promotions', (req, res) => {
     const final =[]
-        const items=[]
+    const items=[]
   
 
-        const newData = req.body;
+    const newData = req.body;
         
-        for (let i=0; i< newData.items.length; i++){
-            console.log(newData.items[i])
-            if (newData.items[i].promotion==undefined || newData.items[i].amount==undefined || newData.items[i].unit_base_price==undefined){
+    for (let i=0; i< newData.items.length; i++){
+        if (newData.items[i].promotion==undefined || newData.items[i].amount==undefined || newData.items[i].unit_base_price==undefined){
     
-                var respuestax=[{
+                let respuestax=[{
                     "status": "NOK",
                     "error_message": "INTERNAL SERVER ERROR"
                     }]
@@ -75,69 +70,48 @@ const promotion_rules = [
                 res.send(respuestax)
                 break
             
-            }else{
             
-                
-                if (newData.items[i].promotion=='Nx$' || newData.items[i].promotion=='AyA'){
+        }else{    
+            if (newData.items[i].promotion=='Nx$' || newData.items[i].promotion=='AyA' ||newData.items[i].promotion==''){
                 if (newData.items[i].amount <= 0 || newData.items[i].unit_base_price <=0){
-                    var respuesta2=[{
+                    let respuesta2=[{
                         "status": "NOK",
                         "error_message": "AMOUNT OR PRICE SHOULD BE GREATER THAN ZERO"
                         }]
                     res.send(respuesta2) 
                 } 
-                else{
-                    
+                else{    
                     if (newData.items[i].promotion=='Nx$'){
-                        if (newData.items[i].amount%cantidad_NProducts==0){
-                            var total_final = promotionNx$(newData.items[i].amount, newData.items[i].unit_base_price);
-                            items.push({
-                                "item_id": newData.items[i].item_id,
-                                "amount": newData.items[i].amount,
-                                "total_price": total_final,
-                                "promotion_applied": true
-                            })
-                        }
-                        else{
-                            var total_final = newData.items[i].amount*newData.items[i].unit_base_price;
-                            items.push({
-                                "item_id": newData.items[i].item_id,
-                                "amount": newData.items[i].amount,
-                                "total_price": total_final,
-                                "promotion_applied": false
-                            })
-
-                        }if (newData.items[i].promotion=='AyA'){
-                            if (newData.items[i].amount==cantidad_AyA){
-                                var total_final = promotionAyA(newData.items[i].amount, newData.items[i].unit_base_price);
-                                items.push({
-                                    "item_id": newData.items[i].item_id,
-                                    "amount": newData.items[i].amount,
-                                    "total_price": total_final,
-                                    "promotion_applied": true
-                                })
-                            }
-                            else{
-                                var total_final = newData.items[i].amount*newData.items[i].unit_base_price;
-                                items.push({
-                                    "item_id": newData.items[i].item_id,
-                                    "amount": newData.items[i].amount,
-                                    "total_price": total_final,
-                                    "promotion_applied": false
-                                })
-    
-                            }
-                        }
-                                               
-                        
+                        let total_price = promotionNx$(newData.items[i].amount, newData.items[i].unit_base_price)
+                        items.push({
+                            "item_id": newData.items[i].item_id,
+                            "amount": newData.items[i].amount,                         
+                            "total_price": total_price,
+                            "promotion_applied": Math.trunc(newData.items[i].amount/cantidad_NProducts)>=1? true:false
+                        })
                     }
-                
-                    
-                }
+                    else if (newData.items[i].promotion=='AyA'){
+                        let total_price = promotionAyA(newData.items[i].amount, newData.items[i].unit_base_price)
+                        items.push({
+                            "item_id": newData.items[i].item_id,
+                            "amount": newData.items[i].amount,                         
+                            "total_price": total_price,
+                            "promotion_applied": Math.trunc(newData.items[i].amount/cantidad_AyA)>=1? true:false
+                        })
+                    }
+                    else{
+                        let total_price = newData.items[i].amount * newData.items[i].unit_base_price
+                        items.push({
+                            "item_id": newData.items[i].item_id,
+                            "amount": newData.items[i].amount,                         
+                            "total_price": total_price,
+                            "promotion_applied": false
+                        })
+            }}
     
                 }   
             else{
-                var respuesta=[{
+                let respuesta=[{
                     "status": "NOK",
                     "error_message": "RULE DOES NOT EXIST"
                     }]
@@ -147,8 +121,8 @@ const promotion_rules = [
             
             
     }
-    var precio_total_t=0;
-                /* console.log(items) */
+    
+                let precio_total_t=0;
                 for (let i=0; i<items.length; i++){
                     precio_total_t += items[i].total_price;
                 }
